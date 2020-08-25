8a69ad5baea198dc4032ec634b3ff6e54fc617c7
vi-vi-io <rscott@vi-vi.io>
2020-08-25 13:08:22 -0400

    preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,14 @@
 $( document ).ready(function() {
+// site preloader
+    // wait until DOM is ready
+    document.addEventListener("DOMContentLoaded", function(event) {
+     // wait until window, stylesheets, images, links, and other media assets are loaded
+     window.onload = function() {
+
+      $('#preloader').fadeOut('slow',function(){$(this).remove();});
+
+     };
+    });
 // nav transform on scroll
     window.addEventListener("scroll", function() {
         if (window.scrollY > 1) {
@@ -114,7 +124,7 @@ $( document ).ready(function() {
     hero_bkgd_tl.to("#sm_bubble_btm", {duration:10, y:25, x:15}, "<");
     hero_bkgd_tl.to("#sm_bubble_lft", {duration:10, y:-15, x:-15}, "<");
 
-    let superhero_tl = gsap.timeline ({delay:.5,});
+    let superhero_tl = gsap.timeline ({delay:0,});
     superhero_tl.from("#superhero",{duration: 1.5, opacity:0, y:150, x:-250, scale:.25});
     // superhero_tl.from("#pane-1", {duration: 1, opacity:0, });
 
